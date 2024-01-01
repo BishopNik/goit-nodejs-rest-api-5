ab57a7fea097fd70380343f94570e5695f4a9be4
Andrew <a.nikanorov@gmail.com>
2024-01-01 22:46:01 +0100

    isEmptyBody

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,11 @@
 /** @format */
 
 const validateBody = require('./validateBody.js');
-const isValidId = require('./isValidId.js');
+const isValidId = require('./isvalidid.js');
 const authenticate = require('./authenticate.js');
 const checkOwner = require('./checkowner.js');
 const upload = require('./upload.js');
+const isEmptyBody = require('./isemptybody.js');
 
 module.exports = {
 	validateBody,
@@ -12,4 +13,5 @@ module.exports = {
 	authenticate,
 	checkOwner,
 	upload,
+	isEmptyBody,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,21 +2,27 @@
 
 const express = require('express');
 
-const { validateBody, authenticate, upload } = require('../../middlewares');
+const { validateBody, authenticate, upload, isEmptyBody } = require('../../middlewares');
 const { registerSchema, loginSchema } = require('../../models');
 const { register, login, logout, getCurrent, changeAvatar } = require('../../controllers/auth');
 const { ctrlWrapper } = require('../../utils');
 
 const authRouter = express.Router();
 
-authRouter.post('/register', validateBody(registerSchema), ctrlWrapper(register));
+authRouter.post('/register', isEmptyBody, validateBody(registerSchema), ctrlWrapper(register));
 
-authRouter.post('/login', validateBody(loginSchema), ctrlWrapper(login));
+authRouter.post('/login', isEmptyBody, validateBody(loginSchema), ctrlWrapper(login));
 
-authRouter.post('/logout', authenticate, ctrlWrapper(logout));
+authRouter.post('/logout', isEmptyBody, authenticate, ctrlWrapper(logout));
 
 authRouter.get('/current', authenticate, ctrlWrapper(getCurrent));
 
-authRouter.patch('/avatar', authenticate, upload.single('avatar'), ctrlWrapper(changeAvatar));
+authRouter.patch(
+	'/avatar',
+	isEmptyBody,
+	authenticate,
+	upload.single('avatar'),
+	ctrlWrapper(changeAvatar)
+);
 
 module.exports = authRouter;
